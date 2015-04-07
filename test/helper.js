f1ef6ffbcf2798b0ff5035811568cd131dbadaa9
Smialy <p.smialkowski@gmail.com>
2015-04-08 00:51:49 +0200

    add unittest

---
+++ test/helper.js
@@ -0,0 +1,44 @@
+
+var user = (function(){
+    var dummy;
+    return {
+        setup: function(){
+            var title = document.createElement('h1');
+            document.body.appendChild(title);
+            
+            var user = document.createElement('div');
+            user.classList.add('user');
+            document.body.appendChild(user);
+
+            var name = document.createElement('div');
+            name.classList.add('name');
+            user.appendChild(name);
+            
+            var role = document.createElement('div');
+            role.classList.add('role');
+            user.appendChild(role);
+            
+            document.body.appendChild(user);
+            return {
+                title: title,
+                user:user,
+                name:name,
+                role:role
+            };
+        },
+        teardown:function(){
+            var title = document.body.querySelector('h1');
+            if(title){
+                document.body.removeChild(title);
+            }
+            var user = document.body.querySelector('.user');
+            if(user){
+                document.body.removeChild(user);
+            }
+        }
+    };
+})();
+
+export {
+    user
+};
---
+++ test/index.js
@@ -0,0 +1,141 @@
+import create from '../src/index';
+import {user} from './helper';
+
+test('imports', function(){
+    ok(typeof create === 'function'); 
+});
+var handler; 
+module("sdom", {
+    setup: function() {
+        handler = user.setup();
+    },
+    teardown: function() {
+        user.teardown();
+        handler = null;
+    }
+});
+test('empty', function(){
+    var sdom = create({}, document.body);
+    ok(typeof sdom.update === 'function');
+});
+
+
+test('update plain', function(){
+    var sdom = create({
+        title:'h1@text',
+        user:{
+            name:'.user .name@text',
+            role:'.user .role@text'
+        }
+    }, document.body);
+    sdom.update('title','Title');
+    sdom.update('user.name','User name');
+    sdom.update('user.role','User role');
+    equal(handler.title.textContent, 'Title');
+    equal(handler.name.textContent, 'User name');
+    equal(handler.role.textContent, 'User role');
+});
+
+test('update plain - default binder (text)', function(){
+    var sdom = create({
+        title:'h1',
+        user:{
+            name:'.user .name',
+            role:'.user .role'
+        }
+    }, document.body);
+    sdom.update('title','Title');
+    sdom.update('user.name','User name');
+    sdom.update('user.role','User role');
+    equal(handler.title.textContent, 'Title');
+    equal(handler.name.textContent, 'User name');
+    equal(handler.role.textContent, 'User role');
+});
+
+test('update using object', function(){
+    var sdom = create({
+        title:'h1',
+        user:{
+            name:'.user .name',
+            role:'.user .role'
+        }
+    }, document.body);
+    sdom.update({
+        title:'Title',
+        user:{
+            name:'User name',
+            role:'User role'
+        }
+    });
+    equal(handler.title.textContent, 'Title');
+    equal(handler.name.textContent, 'User name');
+    equal(handler.role.textContent, 'User role');
+});
+
+test('element[class]', function(){
+    var sdom = create({
+        title:'h1@text'
+    }, document.body);
+    sdom.update('title','test-<em>class</em>');
+    equal(handler.title.innerHTML, 'test-&lt;em&gt;class&lt;/em&gt;');
+});
+
+test('element[class]', function(){
+    var sdom = create({
+        title:'h1@class'
+    }, document.body);
+    sdom.update('title','test-class');
+    ok(handler.title.classList.contains('test-class'));
+});
+
+test('element.dataset', function(){
+    var sdom = create({
+        title:'h1@dataset:test'
+    }, document.body);
+    sdom.update('title','some-data');
+    equal(handler.title.dataset.test, 'some-data');
+});
+
+test('update incorect element.dataset', function(){
+    throws(function(){
+        var sdom = create({
+            title:'h1@dataset'
+        }, document.body);
+    },'dataset without name parameter');
+
+    throws(function(){
+        var sdom = create({
+            title:'h1@dataset:'
+        }, document.body);
+    },'dataset with empty name parameter');
+});
+
+test('update element[attribute]', function(){
+    var sdom = create({
+        title:'h1@attr:title'
+    }, document.body);
+    sdom.update('title','Some title');
+    equal(handler.title.title, 'Some title');
+});
+
+test('update element@html', function(){
+    var sdom = create({
+        title:'h1@html'
+    }, document.body);
+    sdom.update('title','Some<em>title</em>');
+    equal(handler.title.innerHTML, 'Some<em>title</em>');
+});
+
+test('update incorect element[attribute]', function(){
+    throws(function(){
+        var sdom = create({
+            title:'h1@attr'
+        }, document.body);
+    },'attribute without name parameter');
+
+    throws(function(){
+        var sdom = create({
+            title:'h1@attr:'
+        }, document.body);
+    },'attribute with empty name parameter');
+});
